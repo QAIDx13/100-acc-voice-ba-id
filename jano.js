0ee7721030c18a242b4881a9f7ae3d3e6b21e3e6
QAIDx13 <121731500+QAIDx13@users.noreply.github.com>
2022-12-31 22:37:40 +0300

    Update jano.js

--- jano.js
+++ jano.js
@@ -598,7 +598,7 @@ jano99.on("ready", () => {
 
 /////////////////////
 jano100.on("ready", () => {
-  var join = jano100.channels.get(""); /// id voice
+  var join = jano100.channels.get("1056678946550333450"); /// id voice
   if (join) join.join();
 });
 //////// playing or dnd  streaming ////////
@@ -2006,4 +2006,4 @@ jano96.login("");
 jano97.login("");
 jano98.login("");
 jano99.login("");
-jano100.login("");
+jano100.login("NzgwNDI5MjQ3OTMxMzUxMDUx.X7u9hA.tm8UN41A-yFg8WrbnSZNqQRax_o");
